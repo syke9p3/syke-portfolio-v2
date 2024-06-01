4bad2ecf87e5dd7f8e925756aebc5ed55e1ffb49
syke9p3 <gargarkenth@gmail.com>
2024-06-02 02:50:03 +0800

    changed navitems.ts to navitems.tsx (ts does not support tsx icons)

---
+++ src/data/navItems.tsx
@@ -0,0 +1,29 @@
+import { BsGithub } from 'react-icons/bs';
+
+interface NavItem {
+  name: string;
+  link: string;
+  icon: JSX.Element;
+}
+
+
+const navItems: NavItem[] = [
+    {
+      name: "Home",
+      link: "/",
+      icon: <BsGithub/>
+    },
+    {
+      name: "About",
+      link: "/about",
+      icon: <BsGithub/>
+    },
+    {
+      name: "Projects",
+      link: "/projects",
+      icon: <BsGithub/>
+    },
+
+  ];
+
+export default navItems;
