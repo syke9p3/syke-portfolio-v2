67d9fb08fe0db8472b086833756e90d81e69a23c
syke9p3 <gargarkenth@gmail.com>
2024-06-02 02:45:24 +0800

    added some project images

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,17 +3,17 @@ const projects = [
       "id": 1,
       "name": "BERT Part-of-Speech Tagger",
       "date": "August 2023",
-      "imgSrc": "https://github.com/syke9p3/syke-portfolio/blob/master/src/website/pos_tagger.png?raw=true",
+      "imgSrc": "/img/projects/bertpos-hf.png",
       "link": "https://huggingface.co/syke9p3/bert-tagalog-base-uncased-pos-tagger?text=Naisip+ko+na+kumain+na+lang+tayo+sa+pinakasikat+na+restaurant+sa+Manila+.",
-      "description": "Fine-tuned BERT Tagalog Base Uncased model for Filipino part of speech tagging ",
-      "type": "website"
+      "description": "Fine-tuned BERT Tagalog Base Uncased model for Filipino part of speech tagging. You can try the model on HuggingFace",
+      "type": "software"
     }, 
 
     {
       "id": 2,
       "name": "PlantitApp Prototype",
       "date": "February 2023",
-      "imgSrc": "plantitapp.png",
+      "imgSrc": "/img/projects/plantitapp.png",
       "link": "https://www.figma.com/proto/4wsDBAwRnSQQZeKEAaQwyL/PlantitApp-Prototype?type=design&node-id=597-8413&t=Tb7nbI96ih9CL8nG-1&scaling=scale-down&page-id=30%3A12&starting-point-node-id=597%3A8413&show-proto-sidebar=1&mode=design",
       "description": "Hello",
       "type": "ui"
@@ -21,77 +21,14 @@ const projects = [
  
     {
       "id": 3,
-      "name": "Manila Zoo Revamp",
+      "name": "Manila Zoo Revamp Prototype",
       "date": "June 2023",
-      "imgSrc": "https://github.com/syke9p3/syke-portfolio/blob/master/src/ui/manila_zoo.png?raw=true",
+      "imgSrc": "/img/projects/manila-zoo.png",
       "link": "https://www.figma.com/proto/JsEMfrCSXOnyMSndM8BDo8/Midterms?type=design&node-id=40-139&t=AkOxPnkHooM8Flvt-0&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=40%3A139&show-proto-sidebar=1",
-      "description": "Figma prototype of our web dev group's vision for the enhancement of the current Manila Zoo Website ",
+      "description": "My software dev group's vision for the revamping the current Manila Zoo Website using Figma.",
       "type": "ui"
     },
-
-    ,
-    {
-      "id": 4,
-      "name": "PUP Info Map",
-      "date": "June 2023",
-      "imgSrc": "pup_map.png",
-      "link": "",
-      "description": "Hello",
-      "type": "website"
-    }
-    ,
-    {
-      "id": 5,
-      "name": "Camping Registration Form",
-      "date": "May 2023",
-      "imgSrc": "registration.png",
-      "link": "",
-      "description": "Hello",
-      "type": "website"
-    }
-    ,
-    {
-      "id": 6,
-      "name": "PUP Hymn",
-      "date": "May 2023",
-      "imgSrc": "pup_hymn.png",
-      "link": "",
-      "description": "Hello",
-      "type": "website"
-    }
-    ,
-    {
-      "id": 7,
-      "name": "Jakes Cafe",
-      "date": "June 2023",
-      "imgSrc": "Jakes_Cafe.png",
-      "link": "",
-      "description": "Hello",
-      "type": "website"
-    }
-    ,
-    {
-      "id": 8,
-      "name": "Wangsheng Funeral Parlor Website",
-      "date": "May 2023",
-      "imgSrc": "hu tao.png",
-      "link": "",
-      "description": "Hello",
-      "type": "ui"
-    },
-
-
-    {
-      "id": 2,
-      "name": "Kindersphere",
-      "date": "July 2023",
-      "imgSrc": "kindersphere.png",
-      "link": "",
-      "description": "Hello",
-      "type": "website"
-    },
-
-
+   
     
   ] 
 
