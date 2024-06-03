ee7af624589773ad1e23858ba5cae2abdb6f20e1
syke9p3 <gargarkenth@gmail.com>
2024-06-03 18:24:41 +0800

    remove blog page

--- src/data/navItems.tsx
+++ src/data/navItems.tsx
@@ -23,11 +23,11 @@ const navItems: NavItem[] = [
       link: "/projects",
       icon: <BsGithub/>
     },
-    {
-      name: "Blogs",
-      link: "/blogs",
-      icon: <BsGithub/>
-    },
+    // {
+    //   name: "Blogs",
+    //   link: "/blogs",
+    //   icon: <BsGithub/>
+    // },
 
   ];
 
