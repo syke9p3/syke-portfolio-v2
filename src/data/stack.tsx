bd9d4d97f1b2a301e0ec12c79e9a567eb89561dd
syke9p3 <gargarkenth93@gmail.com>
2024-07-03 23:34:41 +0800

    tech stack component

---
+++ src/data/stack.tsx
@@ -0,0 +1,130 @@
+import { BiLogoJquery } from "react-icons/bi";
+import { FaBootstrap, FaCss3Alt, FaFigma, FaGitAlt, FaGithub, FaHtml5, FaJava, FaLinux, FaNodeJs, FaPython, FaReact } from "react-icons/fa6";
+import { IoLogoJavascript } from "react-icons/io";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { SiAdobephotoshop, SiC, SiCplusplus, SiExpress, SiGnubash, SiJupyter, SiMysql, SiPenpot, SiPhp, SiPostman } from "react-icons/si";
+
+export const stack = [
+    {
+        name: 'HTML',
+        icon: <FaHtml5 size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'CSS',
+        icon: <FaCss3Alt size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'Javascript',
+        icon: <IoLogoJavascript size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'PHP',
+        icon: <SiPhp size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'Bootstrap',
+        icon: <FaBootstrap size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'JQuery',
+        icon: <BiLogoJquery size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'React',
+        icon: <FaReact size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'Tailwind',
+        icon: <RiTailwindCssFill size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'NodeJS',
+        icon: <FaNodeJs size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'Express',
+        icon: <SiExpress size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'MySQL',
+        icon: <SiMysql size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'C',
+        icon: <SiC size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'C++',
+        icon: <SiCplusplus size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'Java',
+        icon: <FaJava size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'Python',
+        icon: <FaPython size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'Jupyter',
+        icon: <SiJupyter size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'Postman',
+        icon: <SiPostman size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'Git',
+        icon: <FaGitAlt size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'Github',
+        icon: <FaGithub size={24} />,
+        color: '#3B82F6'
+    },
+
+    {
+        name: 'Figma',
+        icon: <FaFigma size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'Penpot',
+        icon: <SiPenpot size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'Photoshop',
+        icon: <SiAdobephotoshop size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'Linux',
+        icon: <FaLinux size={24} />,
+        color: '#3B82F6'
+    },
+    {
+        name: 'Bash',
+        icon: <SiGnubash size={24} />,
+        color: '#3B82F6'
+    },
+]
+
