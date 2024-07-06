d0313449d4bf572cb6bc7a3ba52b602012ce21b4
syke9p3 <gargarkenth93@gmail.com>
2024-07-06 10:09:53 +0800

    better responsive cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        'cursive': ['Caveat', 'cursive']
+      },
       colors: {
         catppuccinRosewater: "#f5e0dc",
         catppuccinFlamingo: "#f2cdcd",
