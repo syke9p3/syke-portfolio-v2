c849e68d11f765f855ec0b61bb1d4952726c5d55
syke9p3 <gargarkenth93@gmail.com>
2024-07-30 15:36:41 +0800

    404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,15 +1,64 @@
 import { Helmet } from "react-helmet";
+import Terminal from "../components/Window";
+import { Link, useLocation, useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import ResumeButton from "../components/ResumeButton";
+import { CgFile, CgHome } from "react-icons/cg";
 
 const NotFound = () => {
   return (
     <>
       <Helmet>
-        <title>Page Not Found | Kenth Saya-ang</title>
+        <title>Not Found | Kenth Saya-ang</title>
       </Helmet>
-      <main className="min-h-[400px] container mx-auto 80)] bg-cover bg-center bg-no-repeat">
-        <div className="py-12 mx-auto text-4xl font-bold">404 Not found</div>
+      <main className="container mx-auto bg-cover bg-center bg-no-repeat min-h-[80dvh] grid place-items-center">
+        <section className="m-4">
+          <NotFoundTerminal />
+        </section>
       </main>
     </>
+  );
+};
+
+const NotFoundTerminal = () => {
+
+  const location = useLocation();
+  const [pathName, setPathName] = useState('')
+
+  useEffect(() => {
+    setPathName(location.pathname)
+  }, [location]);
+
+  return (
+    <Terminal header="page not found">
+      <div
+        className="items-center p-1 leading-[1.6em] relative"
+        style={{ fontFamily: "Fira Code" }}
+      >
+
+        <div className="flex gap-1 p-4">
+          <section className="overflow-x-scroll gap-2 pb-4 w-full">
+            <p className=" opacity-45">
+              <b className="text-catppuccinGreen font-normal">@syke9p3:~$ </b>
+              <i>find &lt;page&gt; .{pathName} </i>
+            </p>
+            <div className="mt-8">
+              <h1 className="mt-4 font-bold text-3xl">404</h1>
+              <p className="my-4 text-sm">The page you are looking for does not exist.</p>
+              <Link to={'/'} className="inline-block my-4 rounded-full">
+                <button className="flex gap-2 items-center px-4 py-3 text-sm font-semibold bg-gradient rounded-full group text-catppuccinBase">
+                  <div className="group-hover:" ><CgHome size={20} /></div>
+                  Return to Home Page
+                </button>
+              </Link>
+            </div>
+
+
+
+          </section>
+        </div>
+      </div>
+    </Terminal >
   );
 };
 
